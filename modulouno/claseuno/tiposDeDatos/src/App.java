5a9ab607cbab7736062ea4dcb46c1c210b54583e
jorgeStevenR <rodrigesj500@gmail.com>
2025-07-15 12:33:55 -0500

    clase uno ejercicio #1

--- modulouno/claseuno/tiposDeDatos/src/App.java
+++ modulouno/claseuno/tiposDeDatos/src/App.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        
+        //entrada de datos por consola           // = operador de Asignacion 
+
+        Scanner entrada = new Scanner(System.in);
+
+        int edad;
+        String nombre;
+        float estatura;
+        char letra;
+
+        System.out.print("Digite la edad : ");
+        edad = entrada.nextInt();
+        System.out.print("Digite su nombre : ");
+        nombre = entrada.nextLine();
+
+        entrada.nextLine(); //Limpiar el buffer
+        System.out.print("Digite la estatura : ");
+        estatura = entrada.nextFloat();
+        System.out.print("Digire una letra : ");
+        letra = entrada.next().charAt(0);
+
+        //imprimir datos por consola 
+        System.out.print("La edad del estudiante es " + edad 
+                        + " anios" + " el nombre del estudiante es " + nombre 
+                        + " la estatura del estudiante es " + estatura 
+                        + " la letra digitada es " + letra);
+
+
+
     }
 
 }
